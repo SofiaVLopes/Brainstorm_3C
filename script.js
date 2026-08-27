c74c0ee6f12334487b086414099ed43d46bfe095
SofiaL <167090740+SofiaVLopes@users.noreply.github.com>
2026-08-27 09:58:28 -0300

    correção

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ let perguntaAtual;
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
